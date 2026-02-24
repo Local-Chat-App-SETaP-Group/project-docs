a1ec4de479175e12736828846704058effa5db74
miaromo <164634156+miaromo@users.noreply.github.com>
2026-02-24 14:14:59 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Untitled SETaP Project'
+copyright = 'None'
+author = 'Team 7B'
 
 release = '0.1'
 version = '0.1.0'
